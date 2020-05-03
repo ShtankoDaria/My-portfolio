9fe0fa8c5d0ce9987ac01662ae131d2fce660948
Anthony Meirlaen <anthony.meirlaen@gmail.com>
2020-05-03 10:57:44 +0200

    Refactor and cleanup of repo code

--- js/githubapi.js
+++ js/githubapi.js
@@ -1,59 +1,36 @@
 class Repos {
-  //id = "";
+  repositories = [];
 
-  //constructor(convertedResponse) {
-  // Object.assign(this, convertedResponse);
-  //}
-
-  //   getRepos() {
-  //     //const url = `https://api.github.com/repos/ShtankoDaria/${reposId}`;
-  //     const url = `https://api.github.com/repos/ShtankoDaria/duck-duck-clone`;
-  //     return fetch(url)
-  //       .then((response) => response.json())
-  //       .then((data) => {
-  //         this.id = data.id;
-  //         return this;
-  //       });
-  //   }
-  // async function getRepos() {
-  //     clear();
-  //     const url = "https://api.github.com/users/ShtankoDaria/repos?per_page=50";
-  //     const response = await fetch(url);
-  //     const result = await response.json();
-  //     result.items.forEach(i => {
-
-  // const anchor = document.createElement("a")
-  // anchor.href = i.html_url;
-  // anchor.textContent = i.full_name;
-  // divResult.appendChild(anchor)
-  // divResult.appendChild(document.createElement("br"))
-  // })
-  // }
-
-  //   render() {
-  //     const container = document.createElement("div");
-
-  //     container.id = this.id;
-
-  //     return container;
-  //   }
-  //
-  name = "";
-
-  constructor(data) {
-    this.name = data.name;
+  getRepos() {
+    const url = "https://api.github.com/users/ShtankoDaria/repos?per_page=50";
+    return fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        this.repositories = data;
+        return this;
+      });
   }
 
   render() {
-    const repos = document.createElement("p");
-    repos.innerHTML = this.name;
-
     const container = document.createElement("div");
-    container.appendChild(repos);
-    container.id = name;
+
+    console.log(this.repositories);
+
+    this.repositories.map(repository => {
+      const div = document.createElement("div");
+      div.innerHTML = `
+          <div class="col-six">
+            <h6>${repository.name}</h6>
+            <p>
+              ${repository.description}
+            </p>
+          </div>
+      `;
+      container.appendChild(div);
+    })
 
     return container;
   }
 }
 
-//export { Repos };
+export { Repos };
